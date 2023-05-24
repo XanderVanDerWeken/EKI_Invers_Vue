fefc1723f3be6d8c5aa20670a7e27c3c7f6c02c9
xa3687va <xander.van.der.weken@fh-erfurt.de>
2023-05-24 19:56:21 +0200

    Added CORS to Server
    - Needed to add CORS, so VueJs can access data in developer Mode

--- invers_backend/src/main/kotlin/de/fherfurt/invers/InversServer.kt
+++ invers_backend/src/main/kotlin/de/fherfurt/invers/InversServer.kt
@@ -2,12 +2,13 @@ package de.fherfurt.invers
 
 import de.fherfurt.invers.routing.GameRouter.gameRoutes
 import de.fherfurt.invers.routing.PlayerRouter.playerRoutes
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.jetty.*
 import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.response.*
+import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
 
@@ -27,10 +28,18 @@ object InversServer {
 
 fun Application.restApplicationModule() {
     install(ContentNegotiation) {
-        json(Json{
+        json(Json {
             prettyPrint = true
             isLenient = true
         })
+    }
+
+    install(CORS) {
+        allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Get)
+        allowHeader(HttpHeaders.Authorization)
+        anyHost()
     }
 
     routing {
