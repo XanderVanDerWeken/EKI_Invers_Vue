ce903a808b7608b6e176b4c9ae38a24120d4ddb0
xa3687va <xander.van.der.weken@fh-erfurt.de>
2023-05-25 21:39:27 +0200

    Board Rework
    - Made Board work through API Store
    - adapted API Store for board
    - added Shift buttons
    - made it look pretty

--- invers_frontend/src/stores/apiStore.ts
+++ invers_frontend/src/stores/apiStore.ts
@@ -1,9 +1,11 @@
 import { defineStore } from "pinia";
+import type {Piece} from "@/game/model/Piece";
 
 interface State {
     scorePlayerOne: number
     scorePlayerTwo: number
     activePlayer: number
+    board: Piece[]
 }
 
 export const useApiStore = defineStore('api', {
@@ -11,16 +13,25 @@ export const useApiStore = defineStore('api', {
        return {
            scorePlayerOne:0,
            scorePlayerTwo:0,
-           activePlayer:0
+           activePlayer:0,
+           board: []
        }
    },
+    getters: {
+        boardMatrix(): Array<Array<Piece>> {
+            const matrix: Array<Array<Piece>> = [];
+            for(let i = 0; i < 10; i++) {
+                matrix.push(this.board.slice(i * 10, (i + 1) * 10));
+            }
+            return (matrix as Array<Array<Piece>>);
+        },
+    },
     actions: {
        async fetchStats() {
             await fetch('http://localhost:8080/game/stats')
                 .then(response => {
                     response.json()
                         .then(data => {
-                            console.log(data);
                             this.scorePlayerOne = data.scorePlayer1;
                             this.scorePlayerTwo = data.scorePlayer2;
                             this.activePlayer = data.activePlayer;
@@ -29,6 +40,17 @@ export const useApiStore = defineStore('api', {
                 })
                 .catch(error => console.error( error ));
        },
+        async fetchBoard() {
+            await fetch('http://localhost:8080/game/board')
+                .then(response => {
+                    response.json()
+                        .then(data => {
+                            this.board = data;
+                        })
+                        .catch(error => console.error( error ))
+                })
+                .catch(error => console.error( error ));
+        },
        async resetGame() {
            await fetch('http://localhost:8080/game/reset')
                .then(response => {
