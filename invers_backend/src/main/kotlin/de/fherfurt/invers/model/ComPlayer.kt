4bd022abba91530e64993ff3da41532ac12e319f
xa3687va <xander.van.der.weken@fh-erfurt.de>
2023-05-28 21:44:10 +0200

    Updated Return Type for makeMove

--- invers_backend/src/main/kotlin/de/fherfurt/invers/model/ComPlayer.kt
+++ invers_backend/src/main/kotlin/de/fherfurt/invers/model/ComPlayer.kt
@@ -3,7 +3,7 @@ package de.fherfurt.invers.model
 import de.fherfurt.invers.core.Piece
 
 class ComPlayer(piece: Piece, dottedPiece: Piece) : Player(piece, dottedPiece) {
-    override fun makeMove() {
+    override fun makeMove() : Move {
         TODO("Not yet implemented")
     }
 }
